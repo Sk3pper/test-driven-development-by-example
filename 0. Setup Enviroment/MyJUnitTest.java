a99daa626f60d6db84f85bfd6df16d7dc1e6ab0b
Sk3pper <13051136+Sk3pper@users.noreply.github.com>
2025-05-13 19:28:06 +0200

    HelloWorld Example

---
+++ 0. Setup Enviroment/MyJUnitTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyJUnitTest {
+
+    @Test
+    public void testSample() {
+        assertEquals(2 + 2, 4);
+    }
+
+    @Test
+    public void testSample2() {
+        assertEquals(2 + 3, 5);
+    }
+}
