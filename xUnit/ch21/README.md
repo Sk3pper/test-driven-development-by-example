7e6ca92d44f9be993b136e26f335fe9ea28d9b68
Sk3pper <13051136+Sk3pper@users.noreply.github.com>
2025-05-19 16:21:36 +0200

    ch21 added

---
+++ xUnit/ch21/README.md
@@ -0,0 +1,18 @@
+# Run
+```
+❯ python run_12.py 
+```
+
+# To-do list
+- [ ] ~~Invoke test method~~
+- [ ] ~~Invoke setUp first~~**
+- [ ] ~~Invoke tearDown afterward~~
+- [ ] Invoke tearDown even if the test method fails
+- [ ] Run multiple tests
+- [ ] **~~Report collected results~~**
+- [ ] ~~Log string in WasRun~~
+- [ ] Report failed tests **
+
+** New item
+
+# Lessons learned
---
+++ xUnit/ch21/run_12.py
@@ -0,0 +1,51 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+        
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+        self.tearDown()
+        return TestResult()
+        
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+class WasRun(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name=name)
+        
+    def setUp(self):
+        self.log = "setUp "
+        
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+        
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+        
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+        
+    def testTemplateMethod(self):
+        test = WasRun("testMethod") 
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+        
+    def testResult(self):
+        test = WasRun("testMethod") 
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+        
+class TestResult:
+    def summary(self):
+        return "1 run, 0 failed"
+        
+if __name__ == "__main__":
+    TestCaseTest("testTemplateMethod").run()
+    TestCaseTest("testResult").run()
