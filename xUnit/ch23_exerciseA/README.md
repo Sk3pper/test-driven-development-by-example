69655c14b63df5c5e352c630df6945ebf20ce8fc
Sk3pper <13051136+Sk3pper@users.noreply.github.com>
2025-05-20 16:28:49 +0200

    add exercise ch23 - A

---
+++ xUnit/ch23_exerciseA/README.md
@@ -0,0 +1,72 @@
+# Run
+```
+❯ python exercise_23A.py
+```
+
+# To-do list
+- [ ] ~~Invoke test method~~
+- [ ] ~~Invoke setUp first~~**
+- [ ] ~~Invoke tearDown afterward~~
+- [ ] Invoke tearDown even if the test method fails
+- [ ] ~~Run multiple tests~~
+- [ ] ~~Report collected results~~
+- [ ] ~~Log string in WasRun~~
+- [ ] ~~Report failed tests~~
+- [ ] ~~Catch and report setUp errors~~
+- [ ] Create TestSuite from a TestCase class **
+
+** New item
+
+# Exercise: 
+Implement the last two items
+1. [ ] Create TestSuite from a TestCase class 
+2. [x] **~~Invoke tearDown even if the test method fails~~**
+
+### Step 0: Remove setUp OK prints
+After some usage I convinced myself that the `(setUp: OK)` is not necessary. So i remove it leaving only the exception handler in the `TestCase` class during the `self.setUp()` call.
+
+### Step1: Add a little test.
+We start from the last one: **Invoke tearDown even if the test method fails**. From what I see in the code:
+```python
+def run(self, result):
+    result.testStarted()
+
+    try:
+        self.setUp()
+    except Exception as e:
+        if e.args: print(e)
+        result.testFailed()
+
+    try:
+        method = getattr(self, self.name)
+        method()
+    except Exception as e:
+        if e.args: print(e)
+        result.testFailed()
+
+    self.tearDown()
+    return result
+```
+
+When the test method fails the `tearDown()` method is called in any case. Create a test to check it.
+
+```python
+def testTearDownWithTestMethodError(self):
+    test = WasRun("testBrokenMethod") 
+    test.run(self.result)
+    assert("setUp tearDown " == test.log)
+```
+
+The test checks what happens when `testBrokenMethod` is called. If we see the string `"setUp tearDown "` in the log, it means that both the `setUp` and `tearDown` methods were called correctly.
+
+### Step2: Run all tests and fail + Step3: Make a change + Step4: Run the tests and succeed
+In this case the tests do not fail because the current item is already implemented.
+
+```bash
+testTemplateMethod: 		        1 run, 0 failed
+testResult: 			            1 run, 0 failed
+testFailedResultFormatting: 	    1 run, 0 failed
+testFailedResult: 		            1 run, 0 failed
+testSetUpError: 		            1 run, 0 failed
+testTearDownWithTestMethodError: 	1 run, 0 failed
+```
---
+++ xUnit/ch23_exerciseA/exercise_23A.py
@@ -0,0 +1,155 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+        
+    def run(self, result):
+        result.testStarted()
+
+        try:
+            self.setUp()
+        except Exception as e:
+            if e.args: print(e)
+            result.testFailed()
+
+        try:
+            method = getattr(self, self.name)
+            method()
+        except Exception as e:
+            if e.args: print(e)
+            result.testFailed()
+
+        self.tearDown()
+        return result
+        
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.log = ""
+        TestCase.__init__(self, name=name)
+        
+    def setUp(self):
+        self.log = "setUp "
+        
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+        
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+        
+    def testBrokenMethod(self):
+        raise Exception
+    
+class BrokenSetUpWasRun(WasRun):      
+    def setUp(self):
+        raise Exception
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+    
+    def testStarted(self):
+        self.runCount += 1
+        
+    def testFailed(self):
+        self.errorCount += 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+    
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+        
+    def add(self, test):
+        self.tests.append(test)
+        
+    def run(self, result):     
+        for test in self.tests:
+            test.run(result)
+            
+        return result
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod") 
+        test.run(self.result)
+        assert("setUp testMethod tearDown " == test.log)
+        
+    def testResult(self):
+        test = WasRun("testMethod") 
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
+        
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod") 
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
+        
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+        
+    def testSetUpError(self):
+        test = BrokenSetUpWasRun("testMethod") 
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
+        
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
+        
+    def testTearDownWithTestMethodError(self):
+        test = WasRun("testBrokenMethod") 
+        test.run(self.result)
+        assert("setUp tearDown " == test.log)
+        
+if __name__ == "__main__":
+    
+    # suite = TestSuite()
+
+    # suite.add(TestCaseTest("testTemplateMethod"))
+    # suite.add(TestCaseTest("testResult"))
+    # suite.add(TestCaseTest("testFailedResultFormatting"))
+    # suite.add(TestCaseTest("testFailedResult"))
+    # suite.add(TestCaseTest("testSetUpError"))
+    
+    # result = TestResult()
+    # suite.run(result=result)
+    # print(result.summary())
+    
+    res = TestResult()
+    TestCaseTest("testTemplateMethod").run(res)
+    print("testTemplateMethod: \t\t{res}".format(res=res.summary()))
+    
+    res = TestResult()
+    TestCaseTest("testResult").run(res)
+    print("testResult: \t\t\t{res}".format(res=res.summary()))
+    
+    res = TestResult()
+    TestCaseTest("testFailedResultFormatting").run(res)
+    print("testFailedResultFormatting: \t{res}".format(res=res.summary()))
+    
+    res = TestResult()
+    TestCaseTest("testFailedResult").run(res)
+    print("testFailedResult: \t\t{res}".format(res=res.summary()))
+    
+    res = TestResult()
+    TestCaseTest("testSetUpError").run(res)
+    print("testSetUpError: \t\t{res}".format(res=res.summary()))
+    
+    res = TestResult()
+    TestCaseTest("testTearDownWithTestMethodError").run(res)
+    print("testTearDownWithTestMethodError: \t\t{res}".format(res=res.summary()))
